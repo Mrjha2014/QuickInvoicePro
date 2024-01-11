d08c60a3a68f4b0094fa3322fa0321bfd1fc0ff9
ASHUTOSH <bds.ashu@gmail.com>
2024-01-11 22:48:10 +0530

    Add -- tailwind css - In Login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -15,36 +15,39 @@ const LoginComponent = () => {
     if (!result.success) {
       setError(result.message);
     }
-
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
-      </form>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
+        <h2 className="text-2xl font-semibold text-gray-700 text-center">Login</h2>
+        <form onSubmit={handleSubmit} className="mt-4">
+          <div>
+            <label htmlFor="email" className="block text-sm">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 px-4 py-2 w-full border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+            />
+          </div>
+          <div className="mt-4">
+            <label htmlFor="password" className="block text-sm">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-1 px-4 py-2 w-full border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+            />
+          </div>
+          {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
+          <button type="submit" className="w-full mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
